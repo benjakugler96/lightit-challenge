9b279505dfcb7f5029958410317452b92606b2a1
benjamin kugler <benjamin.kugler@benjamin.kugler>
2022-10-30 16:10:29 -0300

    feat: tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#fff",
+      gray: {
+        50: "#f9fafb",
+        100: "#f3f4f6",
+        200: "#e5e7eb",
+        800: "#1f2937",
+      },
+    },
+    screens: {
+      mobile: { max: "425px" },
+      // => @media (max-width: 425px) { ... }
+
+      tablet: { max: "640px" },
+      // => @media (max-width: 640px) { ... }
+
+      laptop: { max: "1024px" },
+      // => @media (max-width: 1024px) { ... }
+    },
+    fontSize: {
+      xs: "0.5rem",
+      sm: "0.75rem",
+      base: "1rem",
+      xl: "1.5rem",
+      xxl: "2rem",
+    },
+  },
+  plugins: [],
+};
