39fb8f3bc50aa05c9c85d35e4eef569440e17b91
benjamin kugler <benjamin.kugler@benjamin.kugler>
2022-10-30 18:53:00 -0300

    fix: run handlers only if in browser

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,4 +1,7 @@
 import { setupWorker } from "msw";
 import { handlers } from "./handlers";
+import { isBrowser } from "../utils/isBrowser";
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers);
+export const worker = isBrowser()
+  ? setupWorker(...handlers)
+  : { start: () => {} };
